92f04a24dd9fc1bc8795e0b74c7bd2e082352c42
aleferamos <alefepdias@gmail.com>
2022-09-15 22:19:51 -0400

    Configurando histrix

--- hr-payroll/src/main/java/com/aleferamos/hrpayroll/resources/PaymentResources.java
+++ hr-payroll/src/main/java/com/aleferamos/hrpayroll/resources/PaymentResources.java
@@ -2,6 +2,7 @@ package com.aleferamos.hrpayroll.resources;
 
 import com.aleferamos.hrpayroll.entities.Payment;
 import com.aleferamos.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,15 @@ public class PaymentResources {
 
     @Autowired
     private PaymentService paymentService;
-
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         return ResponseEntity.ok().body(paymentService.getPayment(workerId, days));
+    }
+
+    ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok().body(payment);
     }
 
 }
